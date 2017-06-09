76259fdbaaac4cbf0403d60a0e28d9b5d83fda11
Junkier <logFM3798@gmail.com>
2017-06-10 00:12:59 +0800

    Keeping going for react_labber

--- react_labber/hello.js
+++ react_labber/hello.js
@@ -1,4 +1,8 @@
+// ReactDOM.render(
+//     <h1> Hello World!!! </h1>  俗稱 JSX
+// );
+
 ReactDOM.render(
-    <h1> Hello World!!!</h1> ,
-    document.getElementById("HiHi!!!")
+  <h1>Hello, world!</h1>,
+  document.getElementById('HiHi')
 );
---
+++ react_labber/output/hello.js
@@ -0,0 +1,9 @@
+// ReactDOM.render(
+//     <h1> Hello World!!! </h1>  俗稱 JSX
+// );
+
+ReactDOM.render(React.createElement(
+  'h1',
+  null,
+  'Hello, world!'
+), document.getElementById('HiHi'));
